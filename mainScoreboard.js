a33d88dd8558370fcb1672b8622b27d6ef860d70
Jachym <jachym.tinavsky@email.cz>
2022-08-27 16:36:24 +0200

    Buttons connected

--- mainScoreboard.js
+++ mainScoreboard.js
@@ -91,6 +91,11 @@ var MainScoreboard = function MainScoreboard(props) {
 	    activeLeagueTab = _useState14[0],
 	    setActiveLeagueTab = _useState14[1];
 
+	var _useState15 = useState(null),
+	    _useState16 = _slicedToArray(_useState15, 2),
+	    buttonsUrl = _useState16[0],
+	    setButtonsUrl = _useState16[1];
+
 	var urlForeignRoot = "//s3-eu-west-1.amazonaws.com/hokej.cz/scoreboard/onlajny/";
 	var urlCzechRoot = "//s3-eu-west-1.amazonaws.com/hokej.cz/scoreboard/";
 
@@ -141,10 +146,19 @@ var MainScoreboard = function MainScoreboard(props) {
 
 	useEffect(function () {
 		if (LeagueTabs.current) {
-			var firstTab = LeagueTabs.current.firstChild.innerHTML.replace("<p>", "").replace("</p>", "");
+			var firstTab = LeagueTabs.current.firstChild.id;
+			console.log(firstTab);
 			setActiveLeagueTab(firstTab);
 		}
 	}, [LeagueTabs.current]);
+
+	useEffect(function () {
+		Object.entries(scoreboardLeagues).map(function (value) {
+			if (value[1].id == activeLeagueTab) {
+				setButtonsUrl(value[1]);
+			}
+		});
+	});
 
 	return React.createElement(
 		"section",
@@ -203,9 +217,10 @@ var MainScoreboard = function MainScoreboard(props) {
 					return React.createElement(
 						"div",
 						{
-							className: "tab-container " + (value.league_name == activeLeagueTab ? "active" : ""),
+							className: "tab-container " + (key == activeLeagueTab ? "active" : ""),
+							id: key,
 							onClick: function onClick() {
-								setActiveLeagueTab(value.league_name);
+								setActiveLeagueTab(key);
 							},
 							key: value.league_name
 						},
@@ -228,9 +243,10 @@ var MainScoreboard = function MainScoreboard(props) {
 						return React.createElement(
 							"div",
 							{
-								className: "tab-container " + (value.league_name == activeLeagueTab ? "active" : ""),
+								className: "tab-container " + (key == activeLeagueTab ? "active" : ""),
+								id: key,
 								onClick: function onClick() {
-									setActiveLeagueTab(value.league_name);
+									setActiveLeagueTab(key);
 								},
 								key: value.league_name
 							},
@@ -252,7 +268,7 @@ var MainScoreboard = function MainScoreboard(props) {
 				    key = _ref6[0],
 				    value = _ref6[1];
 
-				if (value.league_name == activeLeagueTab) {
+				if (key == activeLeagueTab) {
 					return React.createElement(
 						"div",
 						{ key: value.league_name },
@@ -494,7 +510,7 @@ var MainScoreboard = function MainScoreboard(props) {
 				    key = _ref8[0],
 				    value = _ref8[1];
 
-				if (value.league_name == activeLeagueTab) {
+				if (key == activeLeagueTab) {
 					return React.createElement(
 						"div",
 						{ key: value.league_name },
@@ -706,16 +722,319 @@ var MainScoreboard = function MainScoreboard(props) {
 			{ className: "scoreBoard-buttonsContainer" },
 			React.createElement(
 				"a",
-				{ href: "#", className: "scoreBoard-button" },
+				{ href: buttonsUrl ? buttonsUrl.url.matches : "#", className: "scoreBoard-button" },
 				"Rozpis z\xE1pas\u016F"
 			),
 			React.createElement(
 				"a",
-				{ href: "#", className: "scoreBoard-button" },
+				{ href: buttonsUrl ? buttonsUrl.url.table : "#", className: "scoreBoard-button" },
 				"Tabulka sout\u011B\u017Ee"
 			)
 		)
 	);
+};
+
+var scoreboardLeagues = {
+	9000: {
+		id: "12",
+		name: "Mistrovství světa",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "reprezentace/zapasy/15", table: "reprezentace/table/15" }
+	},
+	957: {
+		id: "5884",
+		name: "Repre",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: null, table: null }
+	},
+	950: {
+		id: "2838",
+		name: "Světový pohár",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/reprezentace/zapasy/49", table: "/reprezentace/table/49" }
+	},
+	505: {
+		id: "71",
+		name: "Olympijské hry",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/reprezentace/zapasy/18", table: "/reprezentace/table/18" }
+	},
+	500: {
+		id: "13",
+		name: "Carlson Hockey Games",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/reprezentace/zapasy/21", table: "/reprezentace/table/21" }
+	},
+	100: {
+		id: "101",
+		name: "Tipsport extraliga",
+		nick: "Tipsport ELH",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/tipsport-extraliga/zapasy", table: "/tipsport-extraliga/table" }
+	},
+	90: {
+		id: "0",
+		name: "NHL",
+		nick: null,
+		externalLink: true,
+		sourceOnlajny: false,
+		url: { matches: "/sezona/zapasy", table: "/sezona/table" }
+	},
+	85: {
+		id: "1828",
+		name: "Liga mistrů",
+		nick: "CHL",
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/chl/zapasy", table: "/chl/table" }
+	},
+	80: {
+		id: "82",
+		name: "Chance liga",
+		nick: "Chance liga",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/chance-liga/zapasy", table: "/chance-liga/table" }
+	},
+	77: {
+		id: "3452",
+		name: "2. liga – Sever",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6508",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6508"
+		}
+	},
+	76: {
+		id: "29",
+		name: "2. liga – Střed",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6509",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6509"
+		}
+	},
+	75: {
+		id: "30",
+		name: "2. liga – Východ",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6510",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6510"
+		}
+	},
+	74: {
+		id: "147",
+		name: "Univerzitní liga",
+		nick: "ULLH",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "ullh/zapasy", table: "ullh/table" }
+	},
+	70: {
+		id: "174",
+		name: "turnaj O Pohár DHL",
+		nick: "Pohár DHL",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/56", table: "/mladez/table/56" }
+	},
+	67: {
+		id: "81",
+		name: "REDSTONE Extraliga juniorů",
+		nick: "ELJ",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/4", table: "/mladez/table/4" }
+	},
+	65: {
+		id: "112",
+		name: "ELIOD Extraliga dorostu",
+		nick: "ELD",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/53", table: "/mladez/table/53" }
+	},
+	60: {
+		id: "67",
+		name: "Starší dorost",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/5", table: "/mladez/table/5" }
+	},
+	50: {
+		id: "68",
+		name: "Mladší dorost",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/6", table: "/mladez/table/6" }
+	},
+	48: {
+		id: "51",
+		name: "Liga žen – Final Four",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: {
+			matches: "zensky-hokej/zapasy/10",
+			table: "/zensky-hokej/table/10?table-filter-season=2016&table-filter-competition=6008"
+		}
+	},
+	46: {
+		id: "657",
+		name: "Repre 20",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/31", table: "/mladez/table/31" }
+	},
+	45: {
+		id: "1258",
+		name: "Repre 19",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/35", table: "/mladez/table/35" }
+	},
+	44: {
+		id: "40",
+		name: "Repre 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/32", table: null }
+	},
+	42: {
+		id: "254",
+		name: "Hlinka Gretzky Cup",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/32", table: "/mladez/table/32" }
+	},
+	41: {
+		id: "131",
+		name: "Repre 17",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/33", table: null }
+	},
+	39: {
+		id: "3441",
+		name: "Repre 16",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/34", table: "/mladez/table/34" }
+	},
+	35: {
+		id: "954",
+		name: "MS žen",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/39", table: "/zensky-hokej/table/39" }
+	},
+	25: {
+		id: "405",
+		name: "MS v para hokeji",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "sledge-hokej/zapasy/42", table: "sledge-hokej/table/42" }
+	},
+	22: {
+		id: "781",
+		name: "Repre žen",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/39", table: "/zensky-hokej/table/39" }
+	},
+	20: {
+		id: "658",
+		name: "Repre ženy 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/40", table: null }
+	},
+	19: {
+		id: "3437",
+		name: "Repre ženy 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/40", table: "/zensky-hokej/table/40" }
+	},
+	15: {
+		id: "3162",
+		name: "Repre ženy 16",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/41", table: null }
+	},
+	10: {
+		id: "2849",
+		name: "ZOH mládeže",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/47", table: "/zensky-hokej/table/47" }
+	},
+	8: {
+		id: "2885",
+		name: "Škoda Hockey Cup",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/43", table: null }
+	},
+	7: {
+		id: "63",
+		name: "MMČR 8. tříd",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/44", table: "/mladez/table/44" }
+	},
+	6: {
+		id: "856",
+		name: "MMČR krajů",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/43", table: "/mladez/table/43" }
+	},
+	5: {
+		id: "60",
+		name: "Turnaj výběrů VTM",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/51", table: "/mladez/table/51" }
+	}
 };
 
 var Render = function Render() {
--- src/mainScoreboard.js
+++ src/mainScoreboard.js
@@ -53,6 +53,7 @@ const MainScoreboard = (props) => {
 	const [czechRefetch, setCzechRefetch] = useState(false)
 	const [foreignRefetch, setForeignRefetch] = useState(false)
 	const [activeLeagueTab, setActiveLeagueTab] = useState("")
+	const [buttonsUrl, setButtonsUrl] = useState(null)
 
 	const urlForeignRoot = "//s3-eu-west-1.amazonaws.com/hokej.cz/scoreboard/onlajny/"
 	const urlCzechRoot = "//s3-eu-west-1.amazonaws.com/hokej.cz/scoreboard/"
@@ -92,10 +93,19 @@ const MainScoreboard = (props) => {
 
 	useEffect(() => {
 		if (LeagueTabs.current) {
-			let firstTab = LeagueTabs.current.firstChild.innerHTML.replace("<p>", "").replace("</p>", "")
+			let firstTab = LeagueTabs.current.firstChild.id
+			console.log(firstTab)
 			setActiveLeagueTab(firstTab)
 		}
 	}, [LeagueTabs.current])
+
+	useEffect(() => {
+		Object.entries(scoreboardLeagues).map((value) => {
+			if (value[1].id == activeLeagueTab) {
+				setButtonsUrl(value[1])
+			}
+		})
+	})
 
 	return (
 		<section className="mainScoreboard">
@@ -126,9 +136,10 @@ const MainScoreboard = (props) => {
 							Object.entries(czechQuery.data).map(([key, value]) => {
 								return (
 									<div
-										className={"tab-container " + (value.league_name == activeLeagueTab ? "active" : "")}
+										className={"tab-container " + (key == activeLeagueTab ? "active" : "")}
+										id={key}
 										onClick={() => {
-											setActiveLeagueTab(value.league_name)
+											setActiveLeagueTab(key)
 										}}
 										key={value.league_name}
 									>
@@ -144,9 +155,10 @@ const MainScoreboard = (props) => {
 								if (!isFake) {
 									return (
 										<div
-											className={"tab-container " + (value.league_name == activeLeagueTab ? "active" : "")}
+											className={"tab-container " + (key == activeLeagueTab ? "active" : "")}
+											id={key}
 											onClick={() => {
-												setActiveLeagueTab(value.league_name)
+												setActiveLeagueTab(key)
 											}}
 											key={value.league_name}
 										>
@@ -164,7 +176,7 @@ const MainScoreboard = (props) => {
 				<div className="mainScoreBoard-body">
 					{czechQuery.data != undefined &&
 						Object.entries(czechQuery.data).map(([key, value]) => {
-							if (value.league_name == activeLeagueTab) {
+							if (key == activeLeagueTab) {
 								return (
 									<div key={value.league_name}>
 										{value.matches.map((match) => {
@@ -306,7 +318,7 @@ const MainScoreboard = (props) => {
 						})}
 					{foreignQuery.data != undefined &&
 						Object.entries(foreignQuery.data).map(([key, value]) => {
-							if (value.league_name == activeLeagueTab) {
+							if (key == activeLeagueTab) {
 								return (
 									<div key={value.league_name}>
 										{value.matches.map((match) => {
@@ -432,15 +444,318 @@ const MainScoreboard = (props) => {
 				</div>
 			)}
 			<div className="scoreBoard-buttonsContainer">
-				<a href="#" className="scoreBoard-button">
+				<a href={buttonsUrl ? buttonsUrl.url.matches : "#"} className="scoreBoard-button">
 					Rozpis zápasů
 				</a>
-				<a href="#" className="scoreBoard-button">
+				<a href={buttonsUrl ? buttonsUrl.url.table : "#"} className="scoreBoard-button">
 					Tabulka soutěže
 				</a>
 			</div>
 		</section>
 	)
+}
+
+const scoreboardLeagues = {
+	9000: {
+		id: "12",
+		name: "Mistrovství světa",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "reprezentace/zapasy/15", table: "reprezentace/table/15" },
+	},
+	957: {
+		id: "5884",
+		name: "Repre",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: null, table: null },
+	},
+	950: {
+		id: "2838",
+		name: "Světový pohár",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/reprezentace/zapasy/49", table: "/reprezentace/table/49" },
+	},
+	505: {
+		id: "71",
+		name: "Olympijské hry",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/reprezentace/zapasy/18", table: "/reprezentace/table/18" },
+	},
+	500: {
+		id: "13",
+		name: "Carlson Hockey Games",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/reprezentace/zapasy/21", table: "/reprezentace/table/21" },
+	},
+	100: {
+		id: "101",
+		name: "Tipsport extraliga",
+		nick: "Tipsport ELH",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/tipsport-extraliga/zapasy", table: "/tipsport-extraliga/table" },
+	},
+	90: {
+		id: "0",
+		name: "NHL",
+		nick: null,
+		externalLink: true,
+		sourceOnlajny: false,
+		url: { matches: "/sezona/zapasy", table: "/sezona/table" },
+	},
+	85: {
+		id: "1828",
+		name: "Liga mistrů",
+		nick: "CHL",
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/chl/zapasy", table: "/chl/table" },
+	},
+	80: {
+		id: "82",
+		name: "Chance liga",
+		nick: "Chance liga",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/chance-liga/zapasy", table: "/chance-liga/table" },
+	},
+	77: {
+		id: "3452",
+		name: "2. liga – Sever",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6508",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6508",
+		},
+	},
+	76: {
+		id: "29",
+		name: "2. liga – Střed",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6509",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6509",
+		},
+	},
+	75: {
+		id: "30",
+		name: "2. liga – Východ",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: {
+			matches: "/2-liga/zapasy?matchList-filter-season=2019&matchList-filter-competition=6510",
+			table: "/2-liga/table?table-filter-season=2019&table-filter-competition=6510",
+		},
+	},
+	74: {
+		id: "147",
+		name: "Univerzitní liga",
+		nick: "ULLH",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "ullh/zapasy", table: "ullh/table" },
+	},
+	70: {
+		id: "174",
+		name: "turnaj O Pohár DHL",
+		nick: "Pohár DHL",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/56", table: "/mladez/table/56" },
+	},
+	67: {
+		id: "81",
+		name: "REDSTONE Extraliga juniorů",
+		nick: "ELJ",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/4", table: "/mladez/table/4" },
+	},
+	65: {
+		id: "112",
+		name: "ELIOD Extraliga dorostu",
+		nick: "ELD",
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/53", table: "/mladez/table/53" },
+	},
+	60: {
+		id: "67",
+		name: "Starší dorost",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/5", table: "/mladez/table/5" },
+	},
+	50: {
+		id: "68",
+		name: "Mladší dorost",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/6", table: "/mladez/table/6" },
+	},
+	48: {
+		id: "51",
+		name: "Liga žen – Final Four",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: {
+			matches: "zensky-hokej/zapasy/10",
+			table: "/zensky-hokej/table/10?table-filter-season=2016&table-filter-competition=6008",
+		},
+	},
+	46: {
+		id: "657",
+		name: "Repre 20",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/31", table: "/mladez/table/31" },
+	},
+	45: {
+		id: "1258",
+		name: "Repre 19",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/35", table: "/mladez/table/35" },
+	},
+	44: {
+		id: "40",
+		name: "Repre 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/32", table: null },
+	},
+	42: {
+		id: "254",
+		name: "Hlinka Gretzky Cup",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/32", table: "/mladez/table/32" },
+	},
+	41: {
+		id: "131",
+		name: "Repre 17",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/33", table: null },
+	},
+	39: {
+		id: "3441",
+		name: "Repre 16",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/34", table: "/mladez/table/34" },
+	},
+	35: {
+		id: "954",
+		name: "MS žen",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/39", table: "/zensky-hokej/table/39" },
+	},
+	25: {
+		id: "405",
+		name: "MS v para hokeji",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "sledge-hokej/zapasy/42", table: "sledge-hokej/table/42" },
+	},
+	22: {
+		id: "781",
+		name: "Repre žen",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/39", table: "/zensky-hokej/table/39" },
+	},
+	20: {
+		id: "658",
+		name: "Repre ženy 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/40", table: null },
+	},
+	19: {
+		id: "3437",
+		name: "Repre ženy 18",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/40", table: "/zensky-hokej/table/40" },
+	},
+	15: {
+		id: "3162",
+		name: "Repre ženy 16",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/41", table: null },
+	},
+	10: {
+		id: "2849",
+		name: "ZOH mládeže",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/zensky-hokej/zapasy/47", table: "/zensky-hokej/table/47" },
+	},
+	8: {
+		id: "2885",
+		name: "Škoda Hockey Cup",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/43", table: null },
+	},
+	7: {
+		id: "63",
+		name: "MMČR 8. tříd",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/44", table: "/mladez/table/44" },
+	},
+	6: {
+		id: "856",
+		name: "MMČR krajů",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: true,
+		url: { matches: "/mladez/zapasy/43", table: "/mladez/table/43" },
+	},
+	5: {
+		id: "60",
+		name: "Turnaj výběrů VTM",
+		nick: null,
+		externalLink: false,
+		sourceOnlajny: false,
+		url: { matches: "/mladez/zapasy/51", table: "/mladez/table/51" },
+	},
 }
 
 const Render = () => {
